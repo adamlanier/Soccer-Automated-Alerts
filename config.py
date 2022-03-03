1ad7dc5b6d4d29a34fb8cf35a7f75ccd9cb7eef6
Adam Lanier <adam.lanier@live.com>
2022-03-03 03:13:56 -0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# API keys and other sensitive info
-# be sure that this is in .gitignore
-
-api_key = "2caf6dc0eamshae92ef45af35e27p1755e6jsn60e975492cd6"
-api_host = "api-football-v1.p.rapidapi.com"
